418051a45f3f8649b6930fd28f7d5fed0d5cfdba
musfiqrabby <musfiqrabby69@gmail.com>
2024-03-15 21:29:48 +0600

    header section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Header from './components/Header/Header'
 
 function App() {
   
   return (
     <>
-      <h1 className='text-4xl text-primary'>Assignment Batch 7</h1>
+     <Header></Header>
     </>
   )
 }
